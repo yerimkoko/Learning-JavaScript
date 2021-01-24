a86e36e272686e9dedc5cdfe07a7c4943a730171
yerimkoko <gochi97@naver.com>
2021-01-24 10:33:08 +0900

    3장:프로퍼티와 값

---
+++ index.js
@@ -0,0 +1,11 @@
+const obj = {};
+obj.yellow = "yellow"; // obj에 yellow 프로퍼티 추가..
+
+
+
+let heating = true;
+let cooling = false;
+// 심볼은 다른 어떤 심볼과도 일치하지 않는다. 
+// const RED = Symbol("hi");
+// const BLUE = Symbol("hi")l
+// RED !== BLUE
